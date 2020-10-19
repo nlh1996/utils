e0b5315eb10c9f35d81e1cd8f0080a9c25d45a3d
nlh1996 <1217286494@qq.com>
2020-10-19 13:45:09 +0800

    10_19

--- uuid.go
+++ uuid.go
@@ -13,9 +13,9 @@ func GetUID(tag string) string {
 	duration, _ := time.ParseDuration("8h")
 	now := time.Now().UTC().Add(duration)
 	strTime := now.Format("060102030405")
-	// if index >= 99 {
-	// 	index = 0
-	// }
+	if index >= 99 {
+		index = 0
+	}
 	atomic.AddInt64(&index, 1)
 	uid := tag + strTime + strconv.FormatInt(index, 10)
 	return uid
